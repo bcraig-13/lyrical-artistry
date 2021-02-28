0110679e246e443476d8e211ccadb3b1bed54b7e
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-02-27 17:40:19 -0800

    imported extensive canvas, which was being developed via sandbox; need to get working in React

--- client/src/pages/SearchLyrics.js
+++ client/src/pages/SearchLyrics.js
@@ -1,9 +1,11 @@
 import LyricSearchForm from "../components/SearchLyrics/LyricSearchForm";
+import Canvas from "../components/Canvas";
 
 function SearchLyricsPage() {
     return (
         <div>
             <LyricSearchForm />
+            <Canvas />
         </div>)
 }
 
