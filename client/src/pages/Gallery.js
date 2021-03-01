ccfaf5fb271b18ab7a1d6957bb10a8d6f6b3da19
Brandon Craig <thegamingraven13@gmail.com>
2021-03-01 06:30:50 -0800

    Removed duplicate class from navbar and changed 'class' to 'className'.

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -3,7 +3,7 @@ import React from "react";
 function Gallery() {
   return (
     <div className="card" style={{width: "18rem"}}>
-      <img src="..." className="card-img-top" alt="" style={{width: "200px", height: "200px"}} />
+      <img src="..." className="card-img-top" alt="" style={{width: "18rem", height: "200px"}} />
       <div className="card-body">
         <h5 className="card-title">Test</h5>
       </div>
