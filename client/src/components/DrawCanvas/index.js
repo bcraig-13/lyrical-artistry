f0c286cbebeb54e6dae0b58201ef8f2c083509ab
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-03-01 10:24:33 -0800

    got a input to show up on react canvas; gonna add extra styling variables

--- client/src/components/DrawCanvas/index.js
+++ client/src/components/DrawCanvas/index.js
@@ -1,7 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Canvas from "../Canvas";
 
 function DrawCanvas(props) {
+
+    // vvv might change texts into a state
+    // const [texts, setTexts] = useState([]);
+    // const [textToAdd, setTextToAdd] = useState({});
+    const [textToAdd, setTextToAdd] = useState("testing");
 
     const canvasRef = useRef(null);
     // let canvas;
@@ -14,20 +19,28 @@ function DrawCanvas(props) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(imageObj, 0, 0);
 
+        console.log("draw called");
+        console.log(texts);
 
         for (var i = 0; i < texts.length; i++) {
             var text = texts[i];
+
+
             ctx.fillStyle = text.color;
             ctx.font = `${text.size}px ${text.font}`;
+            // ctx.fillText(text.text, text.x, text.y);
+            // ^^^ will use this in actual font
 
+
+            ctx.fillStyle = "black";
+            ctx.font = "20px arial";
+            ctx.fillText(text, 50, 50);
             // vvv code to check text hitbox
             // ctx.clearRect(text.x, text.y, text.width, text.height);
 
-            ctx.fillText(text.text, text.x, text.y);
         }
     }
 
-    // const draw = (ctx) => { }
 
     var texts = [];
 
@@ -50,44 +63,54 @@ function DrawCanvas(props) {
             y
         },
     ]
-    // var text = {
-    //     text: $("#theText").val(),
-    //     font: $("#theFont").val(),
-    //     size: $("#theSize").val(),
-    //     color: $("#theColor").val(),
-    //     x: 20,
-    //     y: y
-    // };
+
+    function handleTextSubmit() { }
 
 
     useEffect(() => {
 
-
-        
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
         const imageObj = new Image();
         imageObj.src = "./exPhoto.jpg";
 
         imageObj.onload = () => {
-            // ctx.drawImage(imageObj, 0, 0);
-            draw(canvas, ctx, texts, imageObj)
-        }
-        
 
-    }, [])
+            draw(canvas, ctx, [textToAdd], imageObj)
+        }
+
+    }, [textToAdd])
+
+
+    // useEffect(() => {
+
+    //     const canvas = canvasRef.current;
+    //     const ctx = canvas.getContext('2d');
+    //     const imageObj = new Image();
+    //     imageObj.src = "./exPhoto.jpg";
+
+    //     imageObj.onload = () => {
+    //         // ctx.drawImage(imageObj, 0, 0);
+    //         draw(canvas, ctx, texts, imageObj)
+    //     }
+
+
+    // }, [])
 
     return (
         <div>
             {/* <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script> */}
             <h4>Add text to canvas and drag it</h4>
-            <input id="theText" placeholder="text" type="text" />
+            <input onChange={(event) => setTextToAdd(event.target.value)} value={textToAdd} id="theText" placeholder="text" type="text" />
             <input id="theFont" placeholder="font" type="text" />
             <input id="theSize" placeholder="size" type="number" />
             <input id="theColor" placeholder="color" type="text" />
-            <button id="submit">Draw text on canvas</button><br />
+            <button
+                id="submit"
+                onSubmit={() => handleTextSubmit()}
+            >Draw text on canvas</button><br />
             <button id="download">Download</button><br />
-            <canvas id="canvas" ref={canvasRef} {...props} width="3000" height="2500"></canvas>
+            <canvas id="canvas" ref={canvasRef} {...props} width="500" height="500"></canvas>
         </div >
     )
 
