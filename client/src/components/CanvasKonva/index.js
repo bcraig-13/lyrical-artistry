e970b512f63eb7a1816b12feb7890d556aa481ed
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-03-02 10:19:23 -0800

    added code underneath as ref for inverting text color upon drag, not just green (possible enhancement); got code so that can't drag text off canvas (as ref); preparing code to be able to add multiple texts

--- client/src/components/CanvasKonva/index.js
+++ client/src/components/CanvasKonva/index.js
@@ -13,21 +13,19 @@ function CanvasKonva(props) {
   const [image] = useImage(imgSrc);
 
   const [isDragging, setIsDragging] = useState(false);
-  const [currX, setCurrX] = useState(50);
-  const [currY, setCurrY] = useState(50);
-  const [textToAdd, setTextToAdd] = useState("");
-  const [fontToAdd, setFontToAdd] = useState("arial");
-  const [sizeToAdd, setSizeToAdd] = useState(20);
-  const [colorToAdd, setColorToAdd] = useState("black");
+
+  const [inputToAdd, setInputToAdd] = useState({
+    textToAdd: "",
+    fontToAdd: "arial",
+    sizeToAdd: 20,
+    colorToAdd: "black",
+    currX: 50,
+    currY: 50
+  })
 
 
   const width = 500;
   const height = 500;
-
-
-  // imageObj.src = "./exPhoto.jpg";
-
-
 
 
   // const handleExport = () => {
@@ -44,14 +42,30 @@ function CanvasKonva(props) {
   const stageRef = useRef(null);
   // <canvas id="canvas" ref={canvasRef} />
 
+  function handleTextSubmit() {
+
+    // need to push to textsList: ONLY IF THERE'S CONTENT!
+    // need to set inputToAdd to default levels
+    // need to add map function that puts current texts into text components
+    // setTextsList[i]({ ...inputToAdd, currX: e.target.x(), currY: e.target.y() });
+    // can update later so that the index is attached
+    // be sure to add a key!
+  }
+
   return (
     <div>
 
       <h4>Add text to canvas and drag it</h4>
-      <input onChange={(event) => setTextToAdd(event.target.value)} value={textToAdd} id="theText" placeholder="text" type="text" />
-      <input onChange={(event) => setFontToAdd(event.target.value)} value={fontToAdd} id="theFont" placeholder="font" type="text" />
-      <input onChange={(event) => setSizeToAdd(event.target.value)} value={sizeToAdd} id="theSize" placeholder="size" type="number" />
-      <input onChange={(event) => setColorToAdd(event.target.value)} value={colorToAdd} id="theColor" placeholder="color" type="text" />
+      <input onChange={(event) => setInputToAdd({ ...inputToAdd, textToAdd: event.target.value })} value={inputToAdd.textToAdd} id="theText" placeholder="text" type="text" />
+      {/* <input onChange={(event) => setTextToAdd(event.target.value)} value={textToAdd} id="theText" placeholder="text" type="text" /> */}
+      <input onChange={(event) => setInputToAdd({ ...inputToAdd, fontToAdd: event.target.value })} value={inputToAdd.fontToAdd} id="theFont" placeholder="font" type="text" />
+      <input onChange={(event) => setInputToAdd({ ...inputToAdd, sizeToAdd: event.target.value })} value={inputToAdd.sizeToAdd} id="theSize" placeholder="size" type="number" />
+      <input onChange={(event) => setInputToAdd({ ...inputToAdd, colorToAdd: event.target.value })} value={inputToAdd.colorToAdd} id="theColor" placeholder="color" type="text" />
+      <button
+        id="submit"
+        onSubmit={() => handleTextSubmit()}
+      // submit button is gonna add to list of texts
+      >Draw text on canvas</button><br />
 
       <Stage width={width} height={height} ref={stageRef}>
         <Layer>
@@ -59,25 +73,24 @@ function CanvasKonva(props) {
         </Layer>
 
         {/* state for align: center vs align: right would be good too! */}
-        
+
         <Layer>
           <Text
-            text={textToAdd}
-            fontSize={sizeToAdd}
-            fontFamily={fontToAdd}
+            text={inputToAdd.textToAdd}
+            fontSize={inputToAdd.sizeToAdd}
+            fontFamily={inputToAdd.fontToAdd}
 
-            x={currX}
-            y={currY}
+            x={inputToAdd.currX}
+            y={inputToAdd.currY}
 
             draggable
-            fill={isDragging? colorToAdd !=='green'? 'green': 'blue' : colorToAdd}
+            fill={isDragging ? inputToAdd.colorToAdd !== 'green' ? 'green' : 'blue' : inputToAdd.colorToAdd}
             onDragStart={() => {
               setIsDragging(true)
             }}
             onDragEnd={e => {
               setIsDragging(false);
-              setCurrX(e.target.x());
-              setCurrY(e.target.y());
+              setInputToAdd({ ...inputToAdd, currX: e.target.x(), currY: e.target.y() });
             }}
           />
         </Layer>
@@ -99,3 +112,62 @@ export default CanvasKonva;
 //   }
 // }
 // ^^^ might be able to use on the text im dragging? wouldn't hold my breath though...
+
+
+// function colourNameToHex(colour)
+// {
+//     var colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+//     "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
+//     "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
+//     "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
+//     "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
+//     "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
+//     "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
+//     "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
+//     "honeydew":"#f0fff0","hotpink":"#ff69b4",
+//     "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
+//     "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
+//     "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
+//     "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
+//     "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
+//     "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
+//     "navajowhite":"#ffdead","navy":"#000080",
+//     "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
+//     "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
+//     "rebeccapurple":"#663399","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
+//     "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
+//     "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
+//     "violet":"#ee82ee",
+//     "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
+//     "yellow":"#ffff00","yellowgreen":"#9acd32"};
+
+//     if (typeof colours[colour.toLowerCase()] != 'undefined')
+//         return colours[colour.toLowerCase()];
+
+//     return false;
+// }
+
+// function invertColor(hex) {
+//   if (hex.indexOf('#') === 0) {
+//       hex = hex.slice(1);
+//   }
+//   // convert 3-digit hex to 6-digits.
+//   if (hex.length === 3) {
+//       hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+//   }
+//   if (hex.length !== 6) {
+//       throw new Error('Invalid HEX color.');
+//   }
+//   // invert color components
+//   var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+//       g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+//       b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+//   // pad each with zeros and return
+//   return '#' + padZero(r) + padZero(g) + padZero(b);
+// }
+
+// function padZero(str, len) {
+//   len = len || 2;
+//   var zeros = new Array(len).join('0');
+//   return (zeros + str).slice(-len);
+// }
