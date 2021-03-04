aa3a5f3a9391c9a737855ad18c9ec6e314d585a7
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-03-04 06:50:42 -0800

    uncommented code that sends requests to React App- heroku should work nnow

--- server/server.js
+++ server/server.js
@@ -117,9 +117,9 @@ app.use(function (err, req, res, next) {
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get("*", function (req, res) {
-//   res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
-// });
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+});
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
