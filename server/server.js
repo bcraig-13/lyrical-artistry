14d009d9e145edaffcd4d236fc31863191f6aab6
Elijah Davis <elijahgdavis1999@gmail.com>
2021-03-29 13:16:53 -0700

    Added to server

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ var storage = multer.diskStorage({
     cb(null, "uploads");
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now());
+    cb(null, file.filename + "-" + Date.now());
   },
 });
 
